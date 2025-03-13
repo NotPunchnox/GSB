33931473f92d567b85216fc1c7cf9f4028fd043a
NotPunchnox <punchnoxpaypal@gmail.com>
2025-03-13 14:52:50 +0100

    add insert function

---
+++ functions/insert.php
@@ -0,0 +1,24 @@
+<?php include('./logs.php'); ?>
+
+<?php
+    function Insert($request) {
+        // Tentative de connexion à la base de donnée
+        try {
+            $bdd = new PDO("mysql:host=localhost;dbname=gsbV2;charset=utf8", "Admin","AdminSupperSecretPassword");
+        } catch(Exception $e) {
+            // En cas d'erreur lors de la connexion avertir l'utilisateur qu'un problème l'empêche d'acceder à la web app
+            echo "Impossible de se connecter à la base de données.";
+            die('Erreur: ' . $e->getMessage());
+        }
+
+        // Préparer la requête SQL avec prepare pour éviter les injections SQL
+        $response = $bdd->prepare($request);
+        $response->execute();
+        $data = $response->fetch();
+
+        logs(json_encode($data), true);
+
+        return $data;
+    }
+
+?>
